eea7ab0383d3955a108836a8ed53633c1cb6e61d
Brulo7 <lorenzobruni55@gmail.com>
2024-05-31 19:37:37 -0300

    realizamos un programas que segun la edad indica si podemos o no ingresar a la discoteca para practicar algortimos y pseudocodigo

---
+++ Practica-Algoritmos3.py
@@ -0,0 +1,9 @@
+### Paso 1
+edad_requerida = 18
+edad_cliente = int(input("Ingrese la edad del cliente: "))
+
+### Paso 2
+if edad_cliente > 18:
+    print("El cliente tiene: ",edad_cliente,"puede ingresar")
+else: 
+    print("El cliente no cumple con la edad requerida para ingresar")
